2e639408c251cccaafa2441b3bdd66bbbb286214
yashaswinis02 <yashaswinis11423@gmail.com>
2025-09-07 20:55:05 +0530

    ml

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools imports find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='yashaswini',
+author_email='yashaswinis11423@gmail.com',
+packages=find_packages(),
+install_requies=get_requirements('requirements.txt')
+
+)
